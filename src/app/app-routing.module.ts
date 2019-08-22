3d8b35003c648f66bb11b11c854881877b83e51f
praguna <praguna18@hotmail.com>
2019-08-23 00:11:26 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProblemsComponent } from './problems/problems.component';
+import { ProblemDetailComponent } from './problem-detail/problem-detail.component';
+import {NotFoundComponent} from './not-found/not-found.component'
+import { LoginComponent } from './login/login.component';
+import { AuthGuard } from './auth.guard';
+import { RegisterComponent } from './register/register.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'login', pathMatch: 'full' },
+  {path:'login',component:LoginComponent},
+  {path:'register',component:RegisterComponent},
+  {path:'problems',component:ProblemsComponent,canActivate:[AuthGuard]},
+  {path:'problems/:name',component:ProblemDetailComponent,canActivate:[AuthGuard]},
+  {path:'404',component:NotFoundComponent},
+  { path: '**', redirectTo: '/404', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,66 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProblemsComponent } from './problems/problems.component';
+import { ProblemDetailComponent } from './problem-detail/problem-detail.component';
+import { AceEditorModule } from 'ng2-ace-editor';
+import {MatSelectModule} from '@angular/material/select'; 
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { MatInputModule, MatPaginatorModule, MatProgressSpinnerModule, 
+  MatSortModule,MatProgressBarModule, MatTableModule, MatDialogModule } from "@angular/material";
+import { NotFoundComponent } from './not-found/not-found.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AngularSplitModule } from 'angular-split';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
+import { RunCodeComponent } from './run-code/run-code.component';
+import {MatButtonModule} from '@angular/material/button';
+import { SubmitCodeComponent } from './submit-code/submit-code.component';
+import { HttpClientModule } from '@angular/common/http';
+import { SolutionDialogComponent } from './solution-dialog/solution-dialog.component'; 
+import { ExecutionService } from './execution.service';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { TopbarComponent } from './topbar/topbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProblemsComponent,
+    ProblemDetailComponent,
+    NotFoundComponent,
+    RunCodeComponent,
+    SubmitCodeComponent,
+    SolutionDialogComponent,
+    LoginComponent,
+    RegisterComponent,
+    TopbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatTableModule,
+    MatPaginatorModule,
+    AngularFontAwesomeModule,
+    MatSortModule,
+    MatProgressSpinnerModule,
+    MatProgressBarModule,
+    AceEditorModule,
+    MatSelectModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AngularSplitModule.forRoot(),
+    MatDialogModule,
+    MatButtonModule,
+    HttpClientModule,
+    MatProgressSpinnerModule,
+    ReactiveFormsModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [ExecutionService],
+  bootstrap: [AppComponent],
+  entryComponents:[RunCodeComponent,SolutionDialogComponent,SubmitCodeComponent]
 })
 export class AppModule { }
